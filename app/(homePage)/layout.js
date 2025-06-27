b61ee200c4e61fbd31ab18439f5719bc0944792a
AhmEd4004 <hmada.eldsonsy1205@gmail.com>
2025-06-27 20:48:27 +0300

    update

--- app/(homePage)/layout.js
+++ app/(homePage)/layout.js
@@ -1,9 +1,18 @@
 import "./globals.css";
 import NavBar from '@/app/(homePage)/_components/navBar/navBar'
 import SearchInput from "@/app/(homePage)/_components/inputs/searchInput";
+import { headers } from "next/headers";
+
+export default async function RootLayout({ children }) {
+
+  const ua = await headers().get('user-agent') ?? ''
+  const isMobile = /Android|iPhone|iPad|iPod|webOS|BlackBerry|IEMobile|Opera Mini/i.test(ua)
+
+  if (!isMobile) {
+    return (<p>Application for computers is not Available yet!</p>)
+  }
 
 
-export default function RootLayout({ children }) {
   return (
     <>
       <SearchInput></SearchInput>
